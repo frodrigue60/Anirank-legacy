897399aab48695f5633204f527030b35023599c8
Luis Rodz <frodrigue60@gmail.com>
2026-03-11 13:33:17 -0600

    fix(models): add password to fillable array to allow admin user creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'password',
         'score_format_id',
         'slug',
         'avatar',
